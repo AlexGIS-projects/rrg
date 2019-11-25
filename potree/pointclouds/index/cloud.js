1101a8071a7552ece1ea15239fef3b52ad1cf50b
Alex Eberle <alex.eberle@uky.edu>
2019-11-24 22:45:45 -0500

    Added CHR Potree and updated readme

---
+++ potree/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3017633,
+    "boundingBox": {
+        "lx": 5518325.91,
+        "ly": 3835884.49,
+        "lz": 669.28,
+        "ux": 5520325.9,
+        "uy": 3837884.4800000006,
+        "uz": 2669.2700000002233
+    },
+    "tightBoundingBox": {
+        "lx": 5518325.91,
+        "ly": 3835884.49,
+        "lz": 669.28,
+        "ux": 5520325.9,
+        "uy": 3837884.48,
+        "uz": 1298.42
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.32042121887207,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
